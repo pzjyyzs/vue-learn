69c975b84d3964d0d5125d3dfbfdea02bdc57e6b
pzjyyzs <463669395@qq.com>
2023-07-26 22:17:04 +0800

    首页

---
+++ calendar/src/components/daypage/list/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="day-list">
+        <list-item title="当前年月" :content="data['year-month']" />
+        <list-item title="今年属相" :content="data.animalsYear" />
+        <list-item title="农历年份" :content="dat.lunarYear" />
+        <list-item title="适宜事件" :content="data.suit" />
+        <list-item title="避免事件" :content="data.avoid" />
+    </div>
+</template>
+<script>
+import ListItem from './item.vue';
+
+export default {
+    name: 'DayList',
+    props: {
+        data: Object
+    },
+    components: {
+        ListItem
+    }
+}
+</script>
+<style lang="scss" scoped>
+.day-lst {
+    padding: .15rem;
+    box-sizing: border-box;
+}
+</style>
--- calendar/src/views/Day.vue
+++ calendar/src/views/Day.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
         <day-card :data="dayData"></day-card>
+        <day-list :data="dayData"></day-list>
     </div>
 </template>
 
@@ -8,13 +9,15 @@
 import getData from '@/services';
 import { computed, onMounted } from 'vue';
 import DayCard from '@/components/daypage/daycard.vue';
+import DayList from '@/components/daypage/list/index.vue';
 import { useStore } from 'vuex';
 import { getNowDate } from '@/libs/utils';
 
 export default {
     name: 'DayPage',
     components: {
-        DayCard
+        DayCard,
+        DayList
     },
     setup() {
         const store = useStore(),
