18c54a919a0d45c7c4967aa36b9c10041c9d1e4a
pzjyyzs <463669395@qq.com>
2023-12-19 21:28:32 +0800

    ignore dist

--- billing/vite.config.ts
+++ billing/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-learn/billing/dist/',
+  // base: '/vue-learn/billing/dist/', 部署到GitHub才使用
   plugins: [vue()]
 })
