addbbe8d4e3d60c69b5e567d1e206f73b358e518
pzjyyzs <463669395@qq.com>
2023-12-19 22:42:12 +0800

    import vue 4

---
+++ billing/src/App.tsx
@@ -0,0 +1,22 @@
+import { defineComponent, ref } from "vue";
+import { RouterView } from 'vue-router';
+
+export const App = defineComponent({
+    setup() {
+        return () => (<>
+                <header>
+                    <ul>
+                        <li>
+                            <router-link to="/">Foo</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/bar">Bar</router-link>
+                        </li>
+                    </ul>
+                </header>
+                <RouterView />
+                <footer>页脚</footer>
+            </>
+        )
+    }
+})
--- billing/vite.config.ts
+++ billing/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/vue-learn/billing/dist/', 部署到GitHub才使用
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    })
+  ]
 })
