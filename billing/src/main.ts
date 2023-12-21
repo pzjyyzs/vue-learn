8547b965a5ece4f1bf55744272c058250018d189
pzjyyzs <463669395@qq.com>
2023-12-21 22:51:05 +0800

    创建功能界面

--- billing/src/main.ts
+++ billing/src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Foo } from './views/Foo'
-import { Bar } from './views/Bar'
 import { createRouter } from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history'
 
-const router = createRouter({
-    history: history,
-    routes
-})
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
 
